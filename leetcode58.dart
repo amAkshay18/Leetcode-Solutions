aba72b141a18fb7507345573b4853129af00a726
imakshayp18 <thisisakshayp18@gmail.com>
2023-07-12 10:18:29 +0530

    containsDuplicates

---
+++ leetcode58.dart
@@ -0,0 +1,23 @@
+// 58. Length of Last Word
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+// consisting of non-space characters only.
+class Solution {
+  int lengthOfLastWord(String s) {
+    int index = s.length - 1;
+    while (s[index] == " ") {
+      index--;
+    }
+    int count = 0;
+    while (s[index] != " ") {
+      count++;
+      if (s[index] == " " || index == 0) {
+        break;
+      }
+      index--;
+    }
+    return count;
+  }
+}
