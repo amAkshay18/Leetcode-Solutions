cbe63d2b07852f25afb37d3ddc2779f4220c1050
imakshayp18 <thisisakshayp18@gmail.com>
2024-01-05 12:21:38 +0530

    Question number 169 is successfully completed

---
+++ 169.dart
@@ -0,0 +1,40 @@
+// 169. Majority Element
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+
+class Solution {
+  int majorityElement(List<int> nums) {
+    Map<int, int> countMap = {};
+    for (int num in nums) {
+      if (countMap.containsKey(num)) {
+        countMap[num] = countMap[num]! + 1;
+      } else {
+        countMap[num] = 1;
+      }
+    }
+    int majority = 0;
+    int threshold = nums.length ~/ 2;
+    countMap.forEach((key, value) {
+      if (value > threshold) {
+        majority = key;
+      }
+    });
+    return majority;
+  }
+}
