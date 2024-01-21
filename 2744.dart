f96d8dab3a6b6069bd543b16d81804482b7bd0e5
imakshayp18 <thisisakshayp18@gmail.com>
2024-01-21 22:38:52 +0530

    Question number 2744 successfully completedly

---
+++ 2744.dart
@@ -0,0 +1,12 @@
+int maximumNumberOfStringPairs(List<String> words) {
+  int count = 0;
+  for (int i = 0; i < words.length - 1; i++) {
+    for (int j = i + 1; j < words.length; j++) {
+      if (words[i] == words[j] ||
+          words[i] == words[j].split('').reversed.join()) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
