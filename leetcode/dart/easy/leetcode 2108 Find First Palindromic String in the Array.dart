efbf536dc5a280a312c7deae087523cadd128b25
imakshayp18 <thisisakshayp18@gmail.com>
2024-02-14 23:03:06 +0530

    Question number 2108 solved successfully

---
+++ leetcode/dart/easy/leetcode 2108 Find First Palindromic String in the Array.dart
@@ -0,0 +1,8 @@
+String firstPalindrome(List<String> words) {
+  for (int i = 0; i < words.length; i++) {
+    if (words[i] == words[i].split('').reversed.join()) {
+      return words[i];
+    }
+  }
+  return "";
+}
