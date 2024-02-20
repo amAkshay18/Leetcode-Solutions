dba26ccda03eea5f8bd77566c11128ff8acb7a95
imakshayp18 <thisisakshayp18@gmail.com>
2024-02-20 19:52:57 +0530

    Question number 2956 solved successfully

---
+++ leetcode/dart/easy/leetcode 2956 Find Common Elements Between Two Arrays.dart
@@ -0,0 +1,16 @@
+List<int> findIntersectionValues(List<int> nums1, List<int> nums2) {
+  List<int> result = [];
+  int count1 = 0;
+  int count2 = 0;
+  for (int i = 0; i < nums1.length; i++) {
+    if (nums2.contains(nums1[i])) {
+      count1++;
+    }
+  }
+  for (int j = 0; j < nums2.length; j++) {
+    if (nums1.contains(nums2[j])) {
+      count2++;
+    }
+  }
+  return [count1, count2];
+}
