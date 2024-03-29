aea0d54d259a46a454d5e302865700c4bb877e63
imakshayp18 <thisisakshayp18@gmail.com>
2024-03-29 11:14:25 +0530

    Question number1317 solved successfully

---
+++ LeetcodeDartEasy/1317 Convert Integer to the Sum of Two No-Zero Integers.dart
@@ -0,0 +1,7 @@
+List<int> getNoZeroIntegers(int n) {
+  for (int i = 1; i < n; i++) {
+    if (!i.toString().contains('0') && !(n - i).toString().contains('0'))
+      return [i, n - i];
+  }
+  return [];
+}
