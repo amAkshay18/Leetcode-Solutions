875cf44d33dbfca4189bbbe5ad8ef713ed117d69
imakshayp18 <thisisakshayp18@gmail.com>
2024-05-02 13:05:42 +0530

    Question number 682 solved successfully

--- Easy/1 Two Sum.dart
+++ Easy/1 Two Sum.dart
@@ -1,12 +1,22 @@
-class Solution {
-  List<int> twoSum(List<int> nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return [i, j];
-        }
+// Solution 1
+List<int> twoSum(List<int> nums, int target) {
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
       }
     }
-    return [];
   }
+  return [];
+}
+
+// Solution 2
+List<int> twoSum2(List<int> nums, int target) {
+  Map<int, int> numMap = {};
+  for (int i = 0; i < nums.length; i++) {
+    int complement = target - nums[i];
+    if (numMap.containsKey(complement)) return [numMap[complement]!, i];
+    numMap[nums[i]] = i;
+  }
+  return [];
 }
--- Easy/217 Contains Duplicates.dart
+++ Easy/217 Contains Duplicates.dart
@@ -1,13 +1,31 @@
-class Solution {
-  bool containsDuplicate(List<int> nums) {
-    Set<int> set = Set<int>();
-    for (int i = 0; i < nums.length; i++) {
-      if (set.contains(nums[i])) {
-        return true;
-      } else {
-        set.add(nums[i]);
-      }
+// Solution 1
+bool containsDuplicate(List<int> nums) {
+  Set<int> set = Set<int>();
+  for (int i = 0; i < nums.length; i++) {
+    if (set.contains(nums[i])) {
+      return true;
+    } else {
+      set.add(nums[i]);
     }
-    return false;
   }
+  return false;
+}
+
+// Solution 2
+bool containsDuplicates2(List<int> nums) {
+  return nums.toSet().length < nums.length;
+}
+
+// Solution 3
+bool containsDupliates3(List<int> nums) {
+  Map<int, int> countMap = {};
+  for (int num in nums) {
+    countMap[num] = (countMap[num] ?? 0) + 1;
+  }
+  for (int count in countMap.values) {
+    if (count > 1) {
+      return true;
+    }
+  }
+  return false;
 }
