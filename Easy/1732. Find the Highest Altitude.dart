075232d32fd4ffa1bf22729162ea96c610c1f8b8
imakshayp18 <thisisakshayp18@gmail.com>
2024-05-12 23:40:01 +0530

    Question number 1732 solved successfully

---
+++ Easy/1732. Find the Highest Altitude.dart
@@ -0,0 +1,9 @@
+int largestAltitude(List<int> gain) {
+  int altitude = 0;
+  int maxAltitude = 0;
+  for (int i = 0; i < gain.length; i++) {
+    altitude += gain[i];
+    if (altitude > maxAltitude) maxAltitude = altitude;
+  }
+  return maxAltitude;
+}
