705be4c0f512842d06d2d21d9255822f222e6b9c
imakshayp18 <thisisakshayp18@gmail.com>
2024-06-09 21:14:27 +0530

    Question number 290 solved successfully

---
+++ Easy/290. Word Pattern.dart
@@ -0,0 +1,22 @@
+bool wordPatter(String pattern, String s) {
+  List<String> words = s.split(' ');
+  if (pattern.length != words.length) return false;
+  Map<String, String> charToWord = {};
+  Map<String, String> wordToChar = {};
+  for (int i = 0; i < pattern.length; i++) {
+    String char = pattern[i];
+    String word = words[i];
+    if (charToWord.containsKey(char)) {
+      if (charToWord[char] != word) return false;
+    } else {
+      charToWord[char] = word;
+    }
+
+    if (wordToChar.containsKey(word)) {
+      if (wordToChar[word] != char) return false;
+    } else {
+      wordToChar[word] = char;
+    }
+  }
+  return true;
+}
