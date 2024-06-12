2a52ba69ad2b6a3dc0ca518f1bd87ec282435e2a
imakshayp18 <thisisakshayp18@gmail.com>
2024-06-12 20:18:53 +0530

    Question number 75 solved successfully

--- Easy/1122. Relative Sort Array.dart
+++ Easy/1122. Relative Sort Array.dart
@@ -1,6 +1,9 @@
 List<int> relativeSortArray(List<int> arr1, List<int> arr2) {
   Map<int, int> frequencyMap = {};
   for (int num in arr1) {
+    //(frequencyMap[num] ?? 0) + 1:
+    //After determining the value (either the current count or 0 if the key does not exist),
+    //it adds 1 to this value. This increments the count of num by 1.
     frequencyMap[num] = (frequencyMap[num] ?? 0) + 1;
   }
   List<int> result = [];
