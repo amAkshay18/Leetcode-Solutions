fa109378889910e390189b71c044d3748b17f06d
imakshayp18 <thisisakshayp18@gmail.com>
2024-06-28 20:42:33 +0530

    Question number 2176 solved successfully

--- Easy/2778. Sum of Squares of Special Elements.dart
+++ Easy/2778. Sum of Squares of Special Elements.dart
@@ -5,3 +5,11 @@ int sumOfSquares(List<int> nums) {
   }
   return sum;
 }
+
+int sumOfSquares2(List<int> nums) {
+  int sum = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums.length % i + 1 == 0) sum += nums[i] * nums[i];
+  }
+  return sum;
+}
