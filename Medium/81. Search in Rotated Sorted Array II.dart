e6e125f54c92a7ca8c706b322be731bfba712e84
imakshayp18 <thisisakshayp18@gmail.com>
2025-02-01 22:02:34 +0530

    Question number 1232 solved successfully

--- Medium/81. Search in Rotated Sorted Array II.dart
+++ Medium/81. Search in Rotated Sorted Array II.dart
@@ -16,17 +16,35 @@ bool search2(List<int> nums, int target) {
 
 // Solution 3: Binary Search
 bool search3 (List<int> nums, int target) {
-  int left = 0;
-  int right = nums.length - 1;
-  while (left<= right) {
-    int mid = left + (right - left) ~/2;
-    if (nums[mid] == target) {
-      return true;
-    } else if (nums[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+int n = nums.length;
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+      int mid = low + (high - low) ~/ 2;
+
+      if (nums[mid] == target) {
+        return true;
+      }
+
+      if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+        low++;
+        high--;
+      } 
+      // Left sorted
+      else if (nums[low] <= nums[mid]) {
+        if (nums[low] <= target && target <= nums[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      } else {
+        if (nums[mid] <= target && target <= nums[high]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
     }
-  }
-  return false;
+    return false;
 }
