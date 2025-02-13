69e5ef68ef7fd21f4272b0e665d764383dc41633
imakshayp18 <thisisakshayp18@gmail.com>
2025-02-13 23:47:35 +0530

    Question number 1679 solved successfully

---
+++ Medium/1679. Max Number of K-Sum Pairs.dart
@@ -0,0 +1,19 @@
+int maxOperation(List<int> nums, int k) {
+  nums.sort();
+  int left = 0;
+  int right = nums.length - 1;
+  int count = 0;
+  while (left < right) {
+    int sum = nums[left] + nums[right];
+    if (sum == k) {
+      count++;
+      left++;
+      right--;
+    } else if (sum < k) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return count;
+}
