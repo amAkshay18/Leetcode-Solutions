0dd67e8a06d5199be32e12aae74b0ba0325251bb
Akshay P <thisisakshayp18@gmail.com>
2025-12-28 15:27:53 +0530

    Question number 3 solved

---
+++ Medium/3. Longest Substring Without Repeating Characters.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int lengthOfLongestSubstring(String s) {
+    int len = s.length;
+    int i = 0; 
+    int j = 0; 
+    int maxLength = 0; 
+    List<String> substring = [];
+    while (j < len) {
+      if (!substring.contains(s[j])) {
+        substring.add(s[j]);
+        maxLength = maxLength > j - i + 1 ? maxLength : j - i + 1;
+        j++;
+      } else {
+        substring.removeAt(0);
+        i++;
+      }
+    }
+    return maxLength;
+  }
+}
