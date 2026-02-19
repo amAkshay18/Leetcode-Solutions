a69104cfa4b4b0ca69f1cfdeb6471994e3598c77
Akshay P <thisisakshayp18@gmail.com>
2026-02-19 23:57:47 +0530

    Question number 696 solved successfully

---
+++ Easy/696. Count Binary Substrings.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int countBinarySubstrings(String s) {
+    List<int> groups = [];
+    int count = 1;
+    for (int i = 1; i < s.length; i++) {
+      if (s[i] == s[i - 1]) {
+        count++;
+      } else {
+        groups.add(count);
+        count = 1;
+      }
+    }
+    groups.add(count);
+    int result = 0;
+    for (int i = 1; i < groups.length; i++) {
+      result += groups[i] < groups[i - 1] ? groups[i] : groups[i - 1];
+    }
+    return -1;
+  }
+}
