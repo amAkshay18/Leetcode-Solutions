028b25acc1a926360b5a59cd937bc6056cc0382c
Akshay P <thisisakshayp18@gmail.com>
2026-03-06 23:07:33 +0530

    Question number 1784 solved successfully

---
+++ Easy/1784. Check if Binary String Has at Most One Segment of Ones.dart
@@ -0,0 +1,8 @@
+class Solution {
+  bool checkOnesSegment(String s) {
+    for (int i = 0; i < s.length - 1; i++) {
+      if (s[i] == "0" && s[i + 1] == "1") return false;
+    }
+    return true;
+  }
+}
