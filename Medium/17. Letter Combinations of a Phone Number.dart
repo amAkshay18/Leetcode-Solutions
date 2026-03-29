9c1408e4ac426aca7e19790efa835d9aec7edd91
Akshay P <thisisakshayp18@gmail.com>
2026-03-29 20:31:31 +0530

    Question number 17 solved successfully

---
+++ Medium/17. Letter Combinations of a Phone Number.dart
@@ -0,0 +1,37 @@
+class Solution {
+  List<String> letterCombinations(String digits) {
+    if (digits.isEmpty) return [];
+    Map<String, String> mobile = {
+      '2': 'a,b,c',
+      '3': 'd,e,f',
+      '4': 'g,h,i',
+      '5': 'j,k,l',
+      '6': 'm,n,o',
+      '7': 'p,q,r,s',
+      '8': 't,u,v',
+      '9': 'w,x,y,z'
+    };
+    List<List<String>> all = [];
+    for (int i = 0; i < digits.length; i++) {
+      String? val = mobile[digits[i]];›
+      if (val != null) {
+        all.add(val.split(','));
+      }
+    }
+    return _getCombinations(all);
+  }
+
+  List<String> _getCombinations(List<List<String>> input) {
+    List<String> result = [''];
+    for (List<String> group in input) {
+      List<String> temp = [];
+      for (String prefix in result) {
+        for (String letter in group) {
+          temp.add(prefix + letter);
+        }
+      }
+      result = temp;
+    }
+    return result;
+  }
+}
