0208222200fa494194f203f46e73ce6a85d1e53b
Akshay P <thisisakshayp18@gmail.com>
2026-05-07 23:36:20 +0530

    Question number 1221 solved in go successfully

---
+++ Go/1221. Split a String in Balanced Strings.dart
@@ -0,0 +1,15 @@
+func balancedStringSplit(s string) int {
+    balancedCount := 0
+    currentBalance := 0
+    for i := 0; i < len(s); i++ {
+        if s[i] == 'L' {
+            currentBalance++
+        } else if s[i] == 'R' {
+            currentBalance--
+        }
+        if currentBalance == 0 {
+            balancedCount++
+        }
+    }
+    return balancedCount
+}
