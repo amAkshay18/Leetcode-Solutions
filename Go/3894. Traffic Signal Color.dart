d73f031939d027ff1cadc4998308d3e4720dadd7
Akshay P <thisisakshayp18@gmail.com>
2026-05-24 21:48:51 +0530

    Question number 3894 solved successfully

---
+++ Go/3894. Traffic Signal Color.dart
@@ -0,0 +1,10 @@
+// func trafficSignal(timer int) string {
+// 	if timer == 0 {
+// 		return "Green"
+// 	} else if timer == 30 {
+// 		return "Orange"
+// 	} else if timer > 30 && timer <= 90 {
+// 		return "Red"
+// 	}
+// 	return "Invalid"
+// }
