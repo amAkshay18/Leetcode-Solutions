ec5ebffff7bfe18db6a2180130ba0caf0fb4bcba
Akshay P <thisisakshayp18@gmail.com>
2026-06-04 21:38:11 +0530

    Question number 3014 solved in go successfully

---
+++ Go/3014. Minimum Number of Pushes to Type Word I.dart
@@ -0,0 +1,11 @@
+// func minimumPushes(word string) int {
+// 	count := 1
+// 	result := 0
+// 	for i := 1; i <= len(word); i++ {
+// 		result += count
+// 		if i % 8 == 0 {
+// 			count++
+// 		}
+// 	}
+// 	return result
+// }
