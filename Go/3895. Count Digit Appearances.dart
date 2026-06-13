4fd04774705c8f2132ae8256474102dc1b277d5b
Akshay P <thisisakshayp18@gmail.com>
2026-06-13 19:36:33 +0530

    Question number 3895 solved successfully

---
+++ Go/3895. Count Digit Appearances.dart
@@ -0,0 +1,9 @@
+// func countDigitOccurrences(nums []int, digit int) int {
+// 	var sb strings.Builder
+
+// 	for _, n := range nums {
+// 		sb.WriteString(strconv.Itoa(n))
+// 	}
+
+// 	return strings.Count(sb.String(), strconv.Itoa(digit))
+// }
