f19073865a9f26846a765495a1d3615f806875b6
Akshay P <thisisakshayp18@gmail.com>
2026-07-02 17:57:00 +0530

    Question number 1143 solved successfully

---
+++ Medium/1143. Longest Common Subsequence.dart
@@ -0,0 +1,17 @@
+class Solution {
+  int longestCommonSubsequence(String text1, String text2) {
+    int m = text1.length;
+    int n = text2.length;
+    List<List<int>> dp = List.generate(m + 1, (index) => List<int>.filled(n + 1, 0));
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (text1[i - 1] == text2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = dp[i - 1][j].compareTo(dp[i][j - 1]) > 0 ? dp[i - 1][j] : dp[i][j - 1];
+        }
+      }
+    }
+    return dp[m][n];
+  }
+}
