001bfc990daae1313b892cf4831b64a96c2646f0
Akshay P <thisisakshayp18@gmail.com>
2026-07-19 21:51:12 +0530

    Question number 1081 solved successfully

---
+++ Medium/1081. Smallest Subsequence of Distinct Characters.dart
@@ -0,0 +1,26 @@
+class Solution {
+  String smallestSubsequence(String s) {
+    List<int> lastIndex = List.filled(26, 0);
+    for (int i = 0; i < s.length; i++) {
+      lastIndex[s.codeUnitAt(i) - 'a'.codeUnitAt(0)] = i;
+    }
+    List<bool> visited = List.filled(26, false);
+    List<String> stack = [];
+    for (int i = 0; i < s.length; i++) {
+      String ch = s[i];
+      int index = ch.codeUnitAt(0) - 'a'.codeUnitAt(0);
+      if (visited[index]) {
+        continue;
+      }
+      while (stack.isNotEmpty &&
+          stack.last.compareTo(ch) > 0 &&
+          lastIndex[stack.last.codeUnitAt(0) - 'a'.codeUnitAt(0)] > i) {
+        visited[stack.last.codeUnitAt(0) - 'a'.codeUnitAt(0)] = false;
+        stack.removeLast();
+      }
+      stack.add(ch);
+      visited[index] = true;
+    }
+    return stack.join();
+  }
+}
