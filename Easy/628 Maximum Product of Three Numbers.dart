f484554dddcec8113beebeb821920820e96deac7
Akshay P <thisisakshayp18@gmail.com>
2026-07-26 17:18:09 +0530

    Question number 628 solved successfully

--- Easy/628 Maximum Product of Three Numbers.dart
+++ Easy/628 Maximum Product of Three Numbers.dart
@@ -4,7 +4,18 @@ int maximumProduct(List<int> nums) {
   if (nums.length == 3) {
     return nums[0] * nums[1] * nums[2];
   } else {
-    return max(nums[0] * nums[1] * nums[nums.length - 1],
-        nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1]);
+    return max(
+      nums[0] * nums[1] * nums[nums.length - 1],
+      nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1],
+    );
+  }
+}
+class Solution {
+  int maximumProduct(List<int> nums) {
+    nums.sort();
+    int n = nums.length;
+    int product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    int product2 = nums[0] * nums[1] * nums[n - 1];
+    return product1 > product2 ? product1 : product2;
   }
 }
