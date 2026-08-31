b22d156e40a237316653593aba448244b83f5a50
Akshay P <thisisakshayp18@gmail.com>
2026-08-31 23:14:35 +0530

    Question number 1287 solved successfully

---
+++ Easy/1287. Element Appearing More Than 25% In Sorted Array.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int findSpecialInteger(List<int> arr) {
+    int count = 1;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] == arr[i - 1]) {
+        count++;
+      } else {
+        count = 1;
+      }
+      if (count > arr.length / 4) {
+        return arr[i];
+      }
+    }
+    return arr[0];
+  }
+}
